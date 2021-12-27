fcb7c0a4fc0827762911666653eea9360a4e51bf
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2021-12-27 23:08:35 +0000

    Home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+#= HomeController
+#
+# The home controller :)
+class HomeController < ApplicationController
+  def show; end
+end
---
+++ spec/system/home_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'system_helper'
+
+RSpec.describe 'Home', type: :system do
+  it 'is under construction' do
+    visit root_path
+
+    # ICALIA TIP: Use `debug` or `debug binding` instead of `debugger` in system
+    # specs to pause the spec. Open 'localhost:9222' in Chrome to open the
+    # project's browserless app, which should list an option to inspect the
+    # spec's capybara browser session:
+    #
+    # debug binding
+
+    expect(page).to have_content 'Home'
+  end
+end
